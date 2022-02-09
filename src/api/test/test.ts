fe66898ab0a0c54df8208949b6e2550751400bfb
Ricardo Rocha <me@rrocha.uk>
2022-02-09 08:54:40 +0000

    :fire:

--- src/api/test/test.ts
+++ src/api/test/test.ts
@@ -1,9 +1,14 @@
-import {Router} from 'express';
+import {NextFunction, Router} from 'express';
 import multer from 'multer';
 import {storage} from '../../utils/multer';
 import * as util from 'util';
 import * as cp from 'child_process';
 const exec = util.promisify(cp.exec);
+
+import * as fs from 'fs/promises';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const upload = multer({storage});
 
@@ -13,16 +18,17 @@ test.get('/send', (req, res) => {
   res.status(200).send({m: 'sent!'});
 });
 
-test.post('/send', upload.single('project'), async (req, res) => {
+test.post('/send', auth, upload.single('project'), async (req, res) => {
   if (!req.file) {
     res.status(401).send({m: 'error'});
     throw('No File sent.');
   }
   const fileName = req.file!.originalname;
+  const folderName = req.file!.originalname.split('.')[0];
 
-  // await exec('mkdir ~/Desktop/hello');
-  await exec(`cp ${process.cwd()}/uploads/${fileName} ~/Desktop/t`);
-  const {stderr, stdout} = await exec(`unzip -o ~/Desktop/t/${fileName} -d ~/Desktop/hello -x ${fileName}`);
+  await exec(`mkdir /var/www/statics/${folderName}`);
+  // await exec(`cp ${process.cwd()}/uploads/${fileName} /var/www/statics/${folderName}`);
+  const {stderr, stdout} = await exec(`unzip -o ${process.cwd()}/uploads/${fileName} -d /var/www/static/${folderName} -x ${fileName}`);
 
   if (stderr)
     res.status(400).send({stderr: stderr})
@@ -31,3 +37,12 @@ test.post('/send', upload.single('project'), async (req, res) => {
     res.status(200).send({m: 'sent!'});
 });
 
+// temp auth
+async function auth(req: any, res: any, next: any) {
+  const key: string = req.header('api-key');
+  if (key !== process.env.KEY) {
+    return res.status(401).send({m: 'invalid key'});
+  }
+
+  next();
+}
